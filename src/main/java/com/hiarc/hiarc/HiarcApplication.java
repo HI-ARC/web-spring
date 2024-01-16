492142b9c3c332c504f4f80b6cb0c65aaf3cc9d9
tolelom <98kimsungmin@naver.com>
2024-01-17 05:53:58 +0900

    프로젝트 세팅

---
+++ src/main/java/com/hiarc/hiarc/HiarcApplication.java
@@ -0,0 +1,13 @@
+package com.hiarc.hiarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiarcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiarcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiarc/hiarc/HiarcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiarc.hiarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiarcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
